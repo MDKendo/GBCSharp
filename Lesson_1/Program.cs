58f7f246cad57385d4fd6e3188690bafbd3a95f1
DrMartesKendemail <doctorkendo@gmail.com>
2022-08-05 23:46:46 +0500

    Qudratit

--- Lesson_1/Program.cs
+++ Lesson_1/Program.cs
@@ -1,7 +1,24 @@
-﻿Console.Clear();
+﻿// Первая задача
 
-Console.Write("Загадай нормальное целое положительное число: ");
-int number = Convert.ToInt32(Console.ReadLine());
-int sqrt = number * number ;
+// Console.Clear();
 
-Console.WriteLine("Число в квадрате: "+ sqrt);
+// Console.Write("Загадай нормальное целое положительное число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// int sqrt = number * number ;
+
+// Console.WriteLine("Число в квадрате: "+ sqrt);
+
+// Вторая задача
+
+Console.Write("Введи первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введи второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+if (num1 == num2 * num2)
+{
+    Console.Write("Первое число квадрат второго");
+}
+else
+{
+    Console.Write("Первое число не квадрат второго");
+}
