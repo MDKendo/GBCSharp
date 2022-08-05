9ceac144879e87ec51697421d8e196fa4073dce7
DrMartesKendemail <doctorkendo@gmail.com>
2022-08-05 23:16:06 +0500

    Perfect sqrt

--- Lesson_1/Program.cs
+++ Lesson_1/Program.cs
@@ -3,6 +3,5 @@
 Console.Write("Загадай число: ");
 int number = Convert.ToInt32(Console.ReadLine());
 int sqrt = number * number ;
-int sqrt1 = Convert.ToInt32(Math.Pow(number,2));
 
-Console.WriteLine("Число в квадрате: "+ sqrt1);
+Console.WriteLine("Число в квадрате: "+ sqrt);
