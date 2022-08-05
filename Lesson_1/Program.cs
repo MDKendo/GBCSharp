6c331c32466cb575095e5b86123ff70192a5a872
DrMartesKendemail <doctorkendo@gmail.com>
2022-08-05 23:12:41 +0500

    sqrt

--- Lesson_1/Program.cs
+++ Lesson_1/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+
+Console.Write("Загадай число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int sqrt = number * number ;
+int sqrt1 = Convert.ToInt32(Math.Pow(number,2));
+
+Console.WriteLine("Число в квадрате: "+ sqrt1);
