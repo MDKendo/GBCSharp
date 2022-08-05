44582b61bfa316f4be2ae0420488a365406ae8ed
DrMartesKendemail <doctorkendo@gmail.com>
2022-08-05 23:50:53 +0500

    Дни недели

--- Lesson_1/Program.cs
+++ Lesson_1/Program.cs
@@ -10,15 +10,57 @@
 
 // Вторая задача
 
-Console.Write("Введи первое число: ");
-int num1 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введи второе число: ");
-int num2 = Convert.ToInt32(Console.ReadLine());
-if (num1 == num2 * num2)
+// Console.Write("Введи первое число: ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введи второе число: ");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+// if (num1 == num2 * num2)
+// {
+//     Console.Write("Первое число квадрат второго");
+// }
+// else
+// {
+//     Console.Write("Первое число не квадрат второго");
+// }
+
+// Третья задача
+
+Console.Write("Выбери номер дня: ");
+
+int day = Convert.ToInt32(Console.ReadLine());
+
+if (day < 1 || day > 7)
 {
-    Console.Write("Первое число квадрат второго");
+    Console.Write("Ты не знаешь сколько дней в неделе?");
 }
 else
 {
-    Console.Write("Первое число не квадрат второго");
+    if (day == 1)
+    {
+        Console.Write("Понедельник");
+    }
+    if (day == 2)
+    {
+        Console.Write("Вторник");
+    }
+    if (day == 3)
+    {
+        Console.Write("Это среда мой друг");
+    }
+    if (day == 4)
+    {
+        Console.Write("Четверг");
+    }
+    if (day == 5)
+    {
+        Console.Write("Выдыхай, пятница");
+    }
+    if (day == 6)
+    {
+        Console.Write("Суббота");
+    }
+    if (day == 7)
+    {
+        Console.Write("Воскресенье");
+    }
 }
