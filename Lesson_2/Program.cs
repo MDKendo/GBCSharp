0439b829252b31b9feb2302aa8379efb1d54cd69
DrMartesKendemail <doctorkendo@gmail.com>
2022-08-09 22:44:44 +0500

    Lesson 2 1

--- Lesson_2/Program.cs
+++ Lesson_2/Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿int number1 = new Random().Next(10,100);  //последнеее число не входит
+Console.WriteLine(number1);
+
+int num1 = number1/10;
+Console.WriteLine(num1);
+
+int num2 = number1%10;
+Console.WriteLine(num2);
+
+if (num1 > num2)
+{
+    Console.WriteLine(num1);
+}
+else
+{
+    Console.WriteLine(num2);
+}
