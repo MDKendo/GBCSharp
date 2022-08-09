82004cdfaee9e1c0d82922a11447a2497dca5303
DrMartesKendemail <doctorkendo@gmail.com>
2022-08-09 23:42:26 +0500

    Lesson 2 дз 2

--- Lesson_2/Program.cs
+++ Lesson_2/Program.cs
@@ -1,42 +1,61 @@
-﻿// Задача номер 15
+﻿//Задача номер 10
 
-Console.WriteLine("Выходной ли сегодня? ");
-Console.Write("Введи любой номер дня от 1 до 7: ");
+// Console.Write("Введите любое трехзначное число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
 
-int day = Convert.ToInt32(Console.ReadLine());
+// if (number < 100 || number > 1000)
+// {
+//     Console.WriteLine("Нужно трехзначное число, пробуй снова");
+// }
+// else
+// {
+// int num2 = number%100;
+// int num3 = num2/10;
+// Console.WriteLine(num3);
+// }
 
-if (day < 1 || day > 7)
-{
-    Console.WriteLine("Таких дней нет, пробуй снова");
-}
-else
-{
-    if (day == 1)
-    {
-        Console.WriteLine("Понедельник - не выходной");
-    }
-    if (day == 2)
-    {
-        Console.WriteLine("Вторник - не выходной");
-    }
-    if (day == 3)
-    {
-        Console.WriteLine("It is Wednesday, my dudes - не выходной");
-    }
-    if (day == 4)
-    {
-        Console.WriteLine("Четверг - не выходной");
-    }
-    if (day == 5)
-    {
-        Console.WriteLine("Выдыхай, пятница");
-    }
-    if (day == 6)
-    {
-        Console.WriteLine("Суббота - ВЫХОДНОЙ");
-    }
-    if (day == 7)
-    {
-        Console.WriteLine("Воскресенье - ВЫХОДНОЙ");
-    }
-}
+
+
+
+// Задача номер 15
+
+// Console.WriteLine("Выходной ли сегодня? ");
+// Console.Write("Введи любой номер дня от 1 до 7: ");
+
+// int day = Convert.ToInt32(Console.ReadLine());
+
+// if (day < 1 || day > 7)
+// {
+//     Console.WriteLine("Таких дней нет, пробуй снова");
+// }
+// else
+// {
+//     if (day == 1)
+//     {
+//         Console.WriteLine("Понедельник - не выходной");
+//     }
+//     if (day == 2)
+//     {
+//         Console.WriteLine("Вторник - не выходной");
+//     }
+//     if (day == 3)
+//     {
+//         Console.WriteLine("It is Wednesday, my dudes - не выходной");
+//     }
+//     if (day == 4)
+//     {
+//         Console.WriteLine("Четверг - не выходной");
+//     }
+//     if (day == 5)
+//     {
+//         Console.WriteLine("Выдыхай, пятница");
+//     }
+//     if (day == 6)
+//     {
+//         Console.WriteLine("Суббота - ВЫХОДНОЙ");
+//     }
+//     if (day == 7)
+//     {
+//         Console.WriteLine("Воскресенье - ВЫХОДНОЙ");
+//     }
+// }
