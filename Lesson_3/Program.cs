8f8651794f1b640bdf92c9ff5f87b40188b14b56
DrMartesKendemail <doctorkendo@gmail.com>
2022-08-14 15:19:11 +0500

    Lesson 3 1

--- Lesson_3/Program.cs
+++ Lesson_3/Program.cs
@@ -1,1 +1,28 @@
-﻿
+﻿Console.Clear();
+
+// Задача 19: Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+// Console.WriteLine("Палиндром или нет?");
+// Console.WriteLine("Введите любое пятизначное число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// if (number < 0)
+// {
+// number = number * -1;
+// }
+//     if (number > 9999  && number < 100000)
+//     {
+//         string pal = Convert.ToString(number);
+//         if (pal[0] == pal[4] && pal[1] == pal[3])
+//         Console.WriteLine("Это число палиндром");
+//         else
+//     {
+//         Console.WriteLine("Это число не палиндром");
+//     }
+//     }
+
+// else 
+// {
+//     Console.WriteLine("Пятизначное, это из пяти цифр");
+// }
+
