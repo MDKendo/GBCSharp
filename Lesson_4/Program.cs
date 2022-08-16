6c246c1c7ceef8950a9d34a772203e85cd5feb45
DrMartesKendemail <doctorkendo@gmail.com>
2022-08-17 00:39:31 +0500

    Lesson 4 dz 1

--- Lesson_4/Program.cs
+++ Lesson_4/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+
+// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+// Это если по нормальному)
+// Console.WriteLine ("Введите число А: ");
+// int numA = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine ("Введите число B: ");
+// int numB = Convert.ToInt32(Console.ReadLine());
+// if (numA != 0 && numB != 0)
+// {
+//     Console.Write($" Число {numA} в степени {numB} = {Math.Pow(numA,numB)} ");
+// }
+// else 
+// {
+//     Console.Write("Не используйте 0");
+// }
+
+// Это если всетаки цикл
+// Console.WriteLine("Введите число А: ");
+// int numA = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите число B: ");
+// int numB = Convert.ToInt32(Console.ReadLine());
+// int stepen = numA;
+// int count = 1;
+// if (numA != 0 && numB != 0)
+//     while (count <= numB)
+//     {
+//         stepen = stepen * numA;
+//         count++;
+//     }
+// else 
+// {
+//     Console.WriteLine("Не используйте 0");   
+// }
+// Console.WriteLine($" Число {numA} в степени {numB} = {stepen}");
+
+// Задача номер 27. напишите программу, которая принимает на вход число и выдает сумму цифр в числе.
+
