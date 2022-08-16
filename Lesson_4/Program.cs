7093f2ce1e2615261320df8f690242850bcef2bd
DrMartesKendemail <doctorkendo@gmail.com>
2022-08-17 01:20:34 +0500

    Lesson 4 dz 2

--- Lesson_4/Program.cs
+++ Lesson_4/Program.cs
@@ -17,23 +17,43 @@
 // }
 
 // Это если всетаки цикл
-// Console.WriteLine("Введите число А: ");
-// int numA = Convert.ToInt32(Console.ReadLine());
-// Console.WriteLine("Введите число B: ");
-// int numB = Convert.ToInt32(Console.ReadLine());
-// int stepen = numA;
-// int count = 1;
-// if (numA != 0 && numB != 0)
-//     while (count <= numB)
-//     {
-//         stepen = stepen * numA;
-//         count++;
-//     }
-// else 
-// {
-//     Console.WriteLine("Не используйте 0");   
-// }
-// Console.WriteLine($" Число {numA} в степени {numB} = {stepen}");
+Console.WriteLine("Введите число А: ");
+int numA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B: ");
+int numB = Convert.ToInt32(Console.ReadLine());
+int stepen = numA;
+int count = 1;
+double otvet = 0;
+if (numB == 0)
+{
+    Console.WriteLine("Любое число в степени 0 = 1");
+}
+else if (numB > 0)
+{
+    while (count < numB)
+   {
+        stepen = stepen * numA;
+        count++;
+    }
+    Console.WriteLine($" Число {numA} в степени {numB} = {stepen}");
+}
+    else
+{
+     numB = -numB;
+     while (count < numB)
+   {
+        stepen = stepen * numA;
+        count++;
+    }
+    Console.WriteLine($" Число {numA} в степени {-numB} = {(double)1/stepen}");
+}
+
 
 // Задача номер 27. напишите программу, которая принимает на вход число и выдает сумму цифр в числе.
 
+// Console.WriteLine("Введите число: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// string number = Convert.ToString(num);
+// int summa = 0;
+// summa = number [0] + number [1] + number [2] + number [3] + number [4] + number [5];
+// Console.WriteLine(summa);
