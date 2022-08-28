5ec532d1229396696904ca71df3ee16e70ab55b6
DrMartesKendemail <doctorkendo@gmail.com>
2022-08-28 22:23:48 +0500

    Lesson 7 dz

---
+++ Lesson_7/Program.cs
@@ -0,0 +1,44 @@
+﻿Console.Clear();
+
+// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+Console.Write("Ведите количество строк массива: ");
+int stroki = int.Parse(Console.ReadLine());
+
+Console.Write("Ведите количество стобцов массива: ");
+int stolbi = int.Parse(Console.ReadLine());
+
+double [,] array = GetArray(stroki, stolbi);
+PrintArray(array);
+
+double[,] GetArray(int m, int n)
+{
+    double[,] result = new double[m,n];
+    for (int i= 0; i< m; i++)
+    {
+        for (int j = 0; j<n; j++)
+        {
+            result[i,j] = Math.Round((new Random().NextDouble()*2-1.0)*10, 2);
+        }
+    }
+    return result;
+}
+
+void PrintArray(double[,] inArray)
+{
+    for (int i = 0; i<inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+
+// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
