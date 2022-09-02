47f0b0a6a0a72bca125d611d8330ada0b9de0916
DrMartesKendemail <doctorkendo@gmail.com>
2022-09-03 00:59:51 +0500

    Lesson 9 1

--- Lesson_9/Program.cs
+++ Lesson_9/Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿Console.Clear();
+
+// Задача 64: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+
+Console.Write("Введите первое число: ");
+int M = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int N = int.Parse(Console.ReadLine());
+
+PrintNumbers(M,N);
+
+void PrintNumbers(int x, int y)
+{
+    Console.Write($"{x} ");
+    if (x == y) return;
+    PrintNumbers(x+1, y);
+}
