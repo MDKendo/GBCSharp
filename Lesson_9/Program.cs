7c760789069aafc7e938f4e634b4e4ebca3847ba
DrMartesKendemail <doctorkendo@gmail.com>
2022-09-03 01:25:50 +0500

    Lesson 9 2

--- Lesson_9/Program.cs
+++ Lesson_9/Program.cs
@@ -2,16 +2,35 @@
 
 // Задача 64: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
 
+// Console.Write("Введите первое число: ");
+// int M = int.Parse(Console.ReadLine());
+// Console.Write("Введите второе число: ");
+// int N = int.Parse(Console.ReadLine());
+
+// PrintNumbers(M,N);
+
+// void PrintNumbers(int x, int y)
+// {
+//     Console.Write($"{x} ");
+//     if (x == y) return;
+//     PrintNumbers(x+1, y);
+// }
+
+// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
 Console.Write("Введите первое число: ");
 int M = int.Parse(Console.ReadLine());
 Console.Write("Введите второе число: ");
 int N = int.Parse(Console.ReadLine());
+int sum = 0;
 
 PrintNumbers(M,N);
+Console.WriteLine($"Сумма натуральных элементов в промежутке от {M} до {N} = {sum} ");
 
 void PrintNumbers(int x, int y)
 {
-    Console.Write($"{x} ");
+    sum = sum + x;
     if (x == y) return;
     PrintNumbers(x+1, y);
+    
 }
