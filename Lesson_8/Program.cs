ea77cb0e4e386f993b45c0257c90d7682a415650
DrMartesKendemail <doctorkendo@gmail.com>
2022-09-03 04:05:55 +0500

    Lesson 8 3

--- Lesson_8/Program.cs
+++ Lesson_8/Program.cs
@@ -62,64 +62,122 @@
 
 // Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
 
-Console.Write("Ведите количество строк массива: ");
-int stroki = int.Parse(Console.ReadLine());
+// Console.Write("Ведите количество строк массива: ");
+// int stroki = int.Parse(Console.ReadLine());
 
-Console.Write("Ведите количество стобцов массива: ");
-int stolbi = int.Parse(Console.ReadLine());
+// Console.Write("Ведите количество стобцов массива: ");
+// int stolbi = int.Parse(Console.ReadLine());
 
-int [,] array = GetArray(stroki, stolbi);
-PrintArray(array);
+// int [,] array = GetArray(stroki, stolbi);
+// PrintArray(array);
 
-Console.WriteLine($"Строка с наименьшей суммой {(MinSum(array)+1)}");
+// Console.WriteLine($"Строка с наименьшей суммой {(MinSum(array)+1)}");
 
 
-int[,] GetArray(int m, int n)
-{
-    int[,] result = new int[m,n];
-    for (int i= 0; i< m; i++)
-    {
-        for (int j = 0; j<n; j++)
-        {
-            result[i,j] = new Random().Next(0,10);
-        }
-    }
-    return result;
-}
+// int[,] GetArray(int m, int n)
+// {
+//     int[,] result = new int[m,n];
+//     for (int i= 0; i< m; i++)
+//     {
+//         for (int j = 0; j<n; j++)
+//         {
+//             result[i,j] = new Random().Next(0,10);
+//         }
+//     }
+//     return result;
+// }
 
-void PrintArray(int[,] array)
-{
-    for (int i = 0; i<array.GetLength(0); i++)
-    {
-        for (int j = 0; j < array.GetLength(1); j++)
-        {
-            Console.Write($"{array[i,j]} ");
-        }
-        Console.WriteLine();
-    }
-}
+// void PrintArray(int[,] array)
+// {
+//     for (int i = 0; i<array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             Console.Write($"{array[i,j]} ");
+//         }
+//         Console.WriteLine();
+//     }
+// }
 
-int MinSum (int [,] array)
-{
-    int mstroka = 0;
-    int minsumm = 0;
-    for (int i = 0; i<array.GetLength(1); i++)
-    {
-        minsumm = minsumm + array[0,i];
-    }
+// int MinSum (int [,] array)
+// {
+//     int mstroka = 0;
+//     int minsumm = 0;
+//     for (int i = 0; i<array.GetLength(1); i++)
+//     {
+//         minsumm = minsumm + array[0,i];
+//     }
 
-    for (int i = 1; i<array.GetLength(0); i++)
-    {
-        int sum = 0;
-        for (int j = 0; j<array.GetLength(1); j++)
-        {
-            sum = sum + array[i,j];
-        }
-        if (minsumm > sum)
-        {
-            minsumm = sum;
-            mstroka = i;
-        }
-    }
-    return mstroka;
-}
+//     for (int i = 1; i<array.GetLength(0); i++)
+//     {
+//         int sum = 0;
+//         for (int j = 0; j<array.GetLength(1); j++)
+//         {
+//             sum = sum + array[i,j];
+//         }
+//         if (minsumm > sum)
+//         {
+//             minsumm = sum;
+//             mstroka = i;
+//         }
+//     }
+//     return mstroka;
+// }
+
+// Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+
+// Console.WriteLine("Задайте две матрицы");
+// Console.Write("Ведите количество строк матриц: ");
+// int stroki = int.Parse(Console.ReadLine());
+
+// Console.Write("Ведите количество стобцов матриц: ");
+// int stolbi = int.Parse(Console.ReadLine());
+
+// int [,] arrayA = GetArray(stroki, stolbi);
+// int [,] arrayB = GetArray(stroki, stolbi);
+// PrintArray(arrayA);
+// Console.WriteLine();
+// PrintArray(arrayB);
+// Console.WriteLine("Произведение двух матриц");
+// PrintArray(twoMatrix(arrayA,arrayB));
+
+// int[,] GetArray(int m, int n)
+// {
+//     int[,] result = new int[m,n];
+//     for (int i= 0; i< m; i++)
+//     {
+//         for (int j = 0; j<n; j++)
+//         {
+//             result[i,j] = new Random().Next(0,10);
+//         }
+//     }
+//     return result;
+// }
+
+// void PrintArray(int[,] array)
+// {
+//     for (int i = 0; i<array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             Console.Write($"{array[i,j]} ");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+
+// int [,] twoMatrix(int[,] arrayA, int [,] arrayB)
+// {
+//     int [,] arrayC = new int [arrayA.GetLength(0), arrayB.GetLength(1)];
+//     for (int i = 0; i<arrayA.GetLength(0); i++)
+//     {
+//         for (int j = 0; j<arrayB.GetLength(1); j++)
+//         {
+//             for (int k = 0; k<arrayA.GetLength(1); k++)
+//             {
+//                 arrayC[i,j] += arrayA[i,k] * arrayB[k,j];
+//             }
+//         }
+//     }
+//     return arrayC;
+// }
