e0b958889efe37a19537b74bb88ac3918124281b
DrMartesKendemail <doctorkendo@gmail.com>
2022-09-03 06:37:22 +0500

    Lesson 8 5

--- Lesson_8/Program.cs
+++ Lesson_8/Program.cs
@@ -184,61 +184,41 @@
 
 // Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
 
-Console.WriteLine("Введите размеры массива");
-Console.Write("Ведите количество строк массива: ");
-int rows = int.Parse(Console.ReadLine());
-Console.Write("Ведите количество стобцов массива: ");
-int columns = int.Parse(Console.ReadLine());
-Console.Write("Ведите глубину массива: ");
-int  depth = int.Parse(Console.ReadLine());
-//int[,,] array = GetArray(rows, columns, depth);
+Console.Write("Введите размеры массива строки, столбцы, глубина через пробел: ");
+string[] nums = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+int[,,] array = GetArray(new int [] { int.Parse(nums[0]), int.Parse(nums[1]), int.Parse(nums[2])}, 10, 98);
+PrintArray(array);
 
-int [,,] array = new int [rows, columns, depth];
-    for (int i = 0; i<array.GetLength(0); i++)
+int[,,] GetArray(int [] sizes, int min, int max)
+{
+    int [,,] result = new int [sizes[0], sizes[1], sizes[2]];
+    for (int i = 0; i<result.GetLength(0); i++)
     {
-        for (int j = 0; j<array.GetLength(1); j++)
+        for (int j = 0; j<result.GetLength(1); j++)
         {
-            for (int d = 0; d<array.GetLength(1); d++)
+            int k=0;
+            while (k < result.GetLength(2))
             {
-                int element = new Random().Next(10, 99);
-               
+                int element = new Random().Next(min, max +1);
+                if (dublElement(result,element)) continue;
+                result[i,j,k] = element;
+                k++;
             }
         }
     }
+    return result;
 
+}
 
-PrintArray(array);
-
-// int [,,] GetArray(int a, int b, int c)
-// {
-//     int [,,] array = new int [a, b, c];
-//     for (int i = 0; i<array.GetLength(0); i++)
-//     {
-//         for (int j = 0; j<array.GetLength(1); j++)
-//         {
-//             int d=0;
-//             while (d < array.GetLength(2))
-//             {
-//                 int element = new Random().Next(10, 99);
-//                 if (dublElement(array,element)) continue;
-//                 array[i,j,d] = element;
-//                 d++;
-//             }
-//         }
-//     }
-//     return array;
-
-// }
-
-bool dublElement(int[,,] arra, int num)
+bool dublElement(int[,,] array, int num)
 {
     for (int i = 0; i<array.GetLength(0); i++)
     {
         for (int j = 0; j<array.GetLength(1); j++)
         {
-            for (int d = 0; i<array.GetLength(2); d++)
+            for (int k = 0; k<array.GetLength(2); k++)
             {
-                if(array[i,j,d] == num) return true;
+                if(array[i, j, k] == num) return true;
             }
         }
     }
@@ -251,9 +231,9 @@ void PrintArray(int[,,] array)
     {
         for (int j = 0; j < array.GetLength(1); j++)
         {
-            for (int d = 0; d<array.GetLength(2); d++)
+            for (int k = 0; k<array.GetLength(2); k++)
             {
-                Console.Write($"{array[i,j,d]} ({i}, {j}, {d})  ");
+                Console.Write($"{array[i,j,k]} ({i}, {j}, {k})  ");
             }
             Console.WriteLine();
         }
